987ecc8c3e2dfbf5cf052b63c89cce6ea9003fb7
GaspardCulis <gaspard.culis@gmail.com>
2023-06-16 13:58:36 +0200

    API key retreival working + added requirements

--- src/account_generator.py
+++ src/account_generator.py
@@ -4,22 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
 from random import randint
 import requests
 
-BASE_URL = "https://beta.elevenlabs.io/sign-up"
+BASE_URL = "https://beta.elevenlabs.io"
+SIGNUP_URL = "https://beta.elevenlabs.io/sign-up"
 MAIL_DOMAIN = "icznn.com"
 
 def generate_email():
     first_name = names.get_first_name()
     last_name = names.get_last_name()
-    mode = randint(0, 2)
-    if mode == 0:
+    if  randint(0, 1) == 0:
         return f'{first_name}.{last_name}{randint(0, 99)}@{MAIL_DOMAIN}'.lower()
-    elif mode == 1:
-        return f'{first_name}.{last_name.lower()}@{MAIL_DOMAIN}'
-    else:
-        return f'{first_name}{randint(0, 99)}@{MAIL_DOMAIN}'.lower()
+    return f'{first_name}{randint(0, 99)}@{MAIL_DOMAIN}'.lower()
 
 def generate_password():
     password = ""
@@ -55,50 +53,65 @@ def get_confirmation_link(mail: str):
 
 def create_account():
     options = Options()
-    #options.headless = True
+    options.headless = True
     options.add_argument("--disable-logging")
     options.add_argument("--log-level=3")
+    options.add_argument("--window-size=1440,1280")
     driver = webdriver.Chrome(options=options)
-    action = ActionChains(driver)
-    driver.get(BASE_URL)
 
-    sleep(0.5)
+    driver.get(SIGNUP_URL)
 
     email = generate_email()
     password = generate_password()
 
-    cookie_button = driver.find_element(By.ID, "CybotCookiebotDialogBodyButtonAccept")
-    if cookie_button:
-        cookie_button.click()
+    cookie_button = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.ID, "CybotCookiebotDialogBodyButtonAccept"))
+    cookie_button.click()
 
-    email_input = driver.find_element(By.NAME, "email")
-    if not email_input:
-        raise Exception("Email input not found")
+    email_input = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.NAME, "email"))
     email_input.send_keys(email)
 
     password_input = driver.find_element(By.NAME, "password")
-    if not password_input:
-        raise Exception("Password input not found")
     password_input.send_keys(password)
 
-    tos_checkbox = driver.find_element(By.NAME, "terms")
-    if not tos_checkbox:
-        raise Exception("TOS checkbox not found")
+    tos_checkbox =  driver.find_element(By.NAME, "terms")
     tos_checkbox.click()
 
-    sleep(1)
-    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-    if not submit_button:
-        raise Exception("Submit button not found")
+    sleep(0.5)
+    submit_button =  driver.find_element(By.XPATH, "//button[@type='submit']")
     submit_button.click()
-    sleep(10)
 
     link = get_confirmation_link(email)
 
     driver.get(link)
-    sleep(1)
-    driver.close()
+    sleep(2)
+    
+    driver.get(BASE_URL)
 
-    return email, password
+    sign_in_form_button = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, "//a[@data-testid='sign-in-button']"))
+    sign_in_form_button.click()
 
-create_account()
+    email_input = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, "//input[@type='email']"))
+    email_input.send_keys(email)
+
+    password_input = driver.find_element(By.XPATH, "//input[@type='password']")
+    password_input.send_keys(password)
+
+    sleep(0.5)
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button.click()
+
+    account_button = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, "//button[@data-testid='user-menu-button']"))
+    account_button.click()
+
+    profile_button = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, "//a[starts-with(@id, 'headlessui-menu-item')]"))
+    profile_button.click()
+    
+    api_key_input = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, "//input[@type='password']"))
+    sleep(0.5)
+    api_key = api_key_input.get_attribute("value")
+
+    driver.quit()
+
+    return email, password, api_key
+
+print(create_account())
