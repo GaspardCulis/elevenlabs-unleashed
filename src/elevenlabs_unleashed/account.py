a22028f62556988bea4ccc700114af4b4324fea3
GaspardCulis <gaspard@gasdev.fr>
2024-08-17 17:12:58 +0200

    refactor(account): Re-written all code with an OOP and typestate approach

--- src/elevenlabs_unleashed/account.py
+++ src/elevenlabs_unleashed/account.py
@@ -2,20 +2,20 @@ from time import monotonic, sleep
 import names
 import requests
 import undetected_chromedriver as uc
+from selenium.common.exceptions import JavascriptException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from random import randint, sample, shuffle
 import string
-import requests
 import re
 import os
 
 
 BASE_URL = "https://elevenlabs.io"
-SIGNUP_URL = "https://elevenlabs.io/sign-up"
+SIGNUP_URL = f"{BASE_URL}/app/sign-up"
+SIGNIN_URL = f"{BASE_URL}/app/sign-in"
 MAIL_DOMAIN = "dpptd.com"
 
 
@@ -97,6 +97,195 @@ def _get_confirmation_link(mail: str):
     raise Exception("Confirmation link not found")
 
 
+class ElevenLabsWebsite:
+    def __init__(self, driver: uc.Chrome) -> None:
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get(BASE_URL)
+
+    def signup(self):
+        return _ElevenLabsWebsiteSignup(self.driver)
+
+    def signin(self):
+        return _ElevenLabsWebsiteSignin(self.driver)
+
+    def verify_email(self, confirmation_link: str):
+        self.driver.get(confirmation_link)
+        return self
+
+    def check_cookie_banner(self):
+        consent_value = self.driver.execute_script(
+            "return localStorage.getItem('ph_consentDecision')"
+        )
+
+        if consent_value != "false":
+            cookie_button = self.wait.until(
+                EC.element_to_be_clickable(
+                    (By.ID, "CybotCookiebotDialogBodyButtonDecline")
+                )
+            )
+            cookie_button.click()
+        return self
+
+    def sleep(self, seconds: float):
+        sleep(seconds)
+        return self
+
+    def quit(self):
+        self.driver.quit()
+
+
+class _ElevenLabsWebsiteSignup(ElevenLabsWebsite):
+    def __init__(self, driver: uc.Chrome) -> None:
+        super().__init__(driver)
+        self.driver.get(SIGNUP_URL)
+
+    def set_email(self, email: str):
+        email_input = self.wait.until(
+            lambda driver: driver.find_element(By.XPATH, "//input[@type='email']")
+        )
+        email_input.send_keys(email)
+        return self
+
+    def set_password(self, password: str):
+        password_input = self.wait.until(
+            lambda driver: driver.find_element(By.XPATH, "//input[@type='password']")
+        )
+        password_input.send_keys(password)
+        return self
+
+    def check_terms(self):
+        terms_checkbox = self.wait.until(
+            lambda driver: driver.find_element(By.NAME, "terms")
+        )
+        try:
+            self.driver.execute_script(
+                "arguments[0].previousSibling.click();", terms_checkbox
+            )
+        except JavascriptException:
+            self.driver.execute_script("arguments[0].checked = true;", terms_checkbox)
+
+        return self
+
+    def submit(self):
+        submit_button = self.wait.until(
+            lambda driver: driver.find_element(By.TAG_NAME, "form")
+        )
+        submit_button.submit()
+        return super()
+
+    def check_captcha(self):
+        captcha_iframe = self.driver.find_element(By.XPATH, "//iframe[@tabindex='0']")
+
+        self.driver.switch_to.frame(captcha_iframe)
+        captcha_checkbox = self.wait.until(
+            lambda driver: driver.find_element(By.ID, "checkbox")
+        )
+        # Wait for aria-checked to be true
+        t0 = monotonic()
+        while captcha_checkbox.get_attribute("aria-checked") == "false":
+            sleep(0.1)
+            if monotonic() - t0 > 20:
+                raise Exception("Captcha not checked in time")
+
+        self.driver.switch_to.default_content()
+        return self
+
+
+class _ElevenLabsWebsiteSignin(ElevenLabsWebsite):
+    def __init__(self, driver: uc.Chrome) -> None:
+        super().__init__(driver)
+        self.driver.get(SIGNIN_URL)
+
+    def set_email(self, email: str):
+        email_input = self.wait.until(
+            lambda driver: driver.find_element(By.XPATH, "//input[@type='email']")
+        )
+        email_input.send_keys(email)
+
+        return self
+
+    def set_password(self, password: str):
+        password_input = self.wait.until(
+            lambda driver: driver.find_element(By.XPATH, "//input[@type='password']")
+        )
+        password_input.send_keys(password)
+        return self
+
+    def submit(self):
+        submit_button = self.wait.until(
+            lambda driver: driver.find_element(By.TAG_NAME, "form")
+        )
+        submit_button.submit()
+        return _ElevenLabsWebsiteOnBoarding(self.driver)
+
+
+class _ElevenLabsWebsiteOnBoarding(ElevenLabsWebsite):
+    def __init__(self, driver: uc.Chrome) -> None:
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def skip(self):
+        skip_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//button[text()="Skip"]'))
+        )
+        skip_button.click()
+        return _ElevenLabsWebsiteDashboard(self.driver)
+
+
+class _ElevenLabsWebsiteDashboard(ElevenLabsWebsite):
+    def __init__(self, driver: uc.Chrome) -> None:
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def account_menu(self):
+        account_button = self.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@aria-label='Your profile']")
+            )
+        )
+        self.driver.execute_script("arguments[0].click();", account_button)
+        return self
+
+    def profile_and_api_key(self):
+        profile_button = self.wait.until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@aria-label='Profile + API key']")
+            )
+        )
+        profile_button.click()
+        return self
+
+    def refresh_api_key(self):
+        refresh_api_button = self.wait.until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "/html/body/div[3]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div[2]/div/button[2]",
+                )
+            )
+        )
+        refresh_api_button.click()
+
+        confirm_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//button[text()="Confirm"]'))
+        )
+        confirm_button.click()
+        return self
+
+    def get_api_key(self):
+        api_key_input = self.wait.until(
+            lambda driver: driver.find_element(
+                By.XPATH, '//input[@aria-label="API Key"]'
+            )
+        )
+        api_key = ""
+        while api_key == "":
+            api_key = api_key_input.get_attribute("value")
+            sleep(0.1)
+        return api_key
+
+
 def create_account():
     """
     Create an account on Elevenlabs and return the email, password and api key
@@ -106,112 +295,33 @@ def create_account():
     options.add_argument("--disable-logging")
     options.add_argument("--log-level=3")
     options.add_argument("--window-size=1440,1280")
-    driver = uc.Chrome(options=options)
-
-    driver.get(SIGNUP_URL)
 
     email = _generate_email()
     password = _generate_password()
 
-    cookie_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "CybotCookiebotDialogBodyButtonDecline"))
+    labs = ElevenLabsWebsite(uc.Chrome(options=options))
+    api_key = (
+        labs.check_cookie_banner()
+        .signup()
+        .set_email(email)
+        .set_password(password)
+        .check_terms()
+        .sleep(0.5)
+        .check_captcha()
+        .submit()
+        .verify_email(_get_confirmation_link(email))
+        .signin()
+        .set_email(email)
+        .set_password(password)
+        .sleep(0.5)
+        .submit()
+        .skip()
+        .account_menu()
+        .profile_and_api_key()
+        .refresh_api_key()
+        .get_api_key()
     )
-    cookie_button.click()
 
-    email_input = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.XPATH, "//input[@type='email']")
-    )
-    email_input.send_keys(email)
-
-    password_input = driver.find_element(By.XPATH, "//input[@type='password']")
-    password_input.send_keys(password)
-
-    terms_checkbox = driver.find_element(By.NAME, "terms")
-    driver.execute_script("arguments[0].click();", terms_checkbox)
-
-    captcha_iframe = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.XPATH, "//iframe[@tabindex='0']")
-    )
-    driver.switch_to.frame(captcha_iframe)
-    captcha_checkbox = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.ID, "checkbox")
-    )
-    # Wait for aria-checked to be true
-    t0 = monotonic()
-    while captcha_checkbox.get_attribute("aria-checked") == "false":
-        sleep(0.1)
-        if monotonic() - t0 > 20:
-            raise Exception("Captcha not checked in time")
-
-    driver.switch_to.default_content()
-
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
-    )
-    submit_button.click()
-
-    link = _get_confirmation_link(email)
-
-    driver.get(link)
-
-    close_button = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.XPATH, "//button[text()='Close']")
-    )
-    close_button.click()
-
-    email_input = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.XPATH, "//input[@type='email']")
-    )
-    email_input.send_keys(email)
-
-    password_input = driver.find_element(By.XPATH, "//input[@type='password']")
-    password_input.send_keys(password)
-
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
-    )
-    submit_button.click()
-
-    skip_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//button[text()="Skip"]'))
-    )
-    skip_button.click()
-
-    account_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Your profile']"))
-    )
-    account_button.click()
-
-    profile_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//button[@aria-label='Profile + API key']")
-        )
-    )
-    profile_button.click()
-
-    refresh_api_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(
-            (
-                By.XPATH,
-                "/html/body/div[3]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div[2]/div/button[2]",
-            )
-        )
-    )
-    refresh_api_button.click()
-
-    confirm_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//button[text()="Confirm"]'))
-    )
-    confirm_button.click()
-
-    api_key_input = WebDriverWait(driver, 10).until(
-        lambda driver: driver.find_element(By.XPATH, '//input[@aria-label="API Key"]')
-    )
-    api_key = ""
-    while api_key == "":
-        api_key = api_key_input.get_attribute("value")
-        sleep(0.1)
-
-    driver.quit()
+    labs.quit()
 
     return email, password, api_key
